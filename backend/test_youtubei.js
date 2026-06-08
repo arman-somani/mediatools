a55d483403ad06ee79547bd129957fb63542430e
arman-somani <armansomani786@gmail.com>
2026-06-08 13:35:28 +0530

    Auto-sync updates

--- backend/test_youtubei.js
+++ backend/test_youtubei.js
@@ -17,18 +17,11 @@ Platform.shim.eval = (script) => {
     
     console.log('Created! Fetching video...');
     const stream = await yt.download('jNQXAC9IVRw', {
-      type: 'audio',
+      type: 'video+audio',
       quality: 'best',
       format: 'mp4'
     });
     console.log('Stream works!', !!stream);
-    
-    const fileStream = fs.createWriteStream('test-download.mp4');
-    for await (const chunk of stream) {
-      fileStream.write(chunk);
-    }
-    fileStream.end();
-    console.log('Finished writing to test-download.mp4');
   } catch(e) {
     console.error(e);
   }
