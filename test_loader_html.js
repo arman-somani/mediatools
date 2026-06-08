d0c540d07f637f5520b601a300e8f2a4a0b6a70d
arman-somani <armansomani786@gmail.com>
2026-06-08 15:32:40 +0530

    Auto-sync updates

--- test_loader_html.js
+++ test_loader_html.js
@@ -1,4 +1,4 @@
 (async () => {
-  const r = await fetch('https://dave15.savenow.to/pacific/?eHiXKcrS5UVOzjOS2Munlrb');
+  const r = await fetch('https://179-43-150-154.nip.io/pacific/?eHiXKcrS5UVOzjOS2Munlrb');
   console.log(await r.text());
 })();
